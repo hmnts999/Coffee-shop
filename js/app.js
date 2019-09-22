593bec000daa3f9cb6557177d443ec8c27d4096b
Hemant singh <37107064+hmnts999@users.noreply.github.com>
2019-09-22 22:34:41 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //constructor function
-function UI() {}
 
 // hide preloader
 // all the imaged scripts links have finished loading
@@ -31,6 +30,8 @@ function eventListeners() {
 			let value = ui.checkEmpty(name, lastName, email);
 
 			if (value) {
+				let customer = new Customer(name, lastName, email);
+				ui.addCustomer(customer);
 				ui.showFeedback("customer added to the list", "success");
 			} else {
 				ui.showFeedback("some form values empty", "error");
@@ -38,21 +39,24 @@ function eventListeners() {
 		});
 }
 
+//constructor function
 function UI() {}
 
+// hide preloader
 UI.prototype.hidePreloader = function() {
 	document.querySelector(".preloader").style.display = "none";
 };
 
+// show Nav
 UI.prototype.showNav = function() {
 	document.querySelector(".nav").classList.toggle("nav--show");
 };
 
 //check the empty values
-UI.prototype.checkEmpty = function(name, lastName, email) {
+UI.prototype.checkEmpty = function(name, lastname, email) {
 	let result;
 
-	if (name === "" || lastName === "" || email === "") {
+	if (name === "" || lastname === "" || email === "") {
 		result = false;
 	} else {
 		result = true;
@@ -79,3 +83,23 @@ UI.prototype.removeAlert = function(type) {
 		document.querySelector(".drink-form__feedback").classList.remove(type);
 	}, 3000);
 };
+
+// add customer
+UI.prototype.addCustomer = function(customer) {
+	const images = [1, 2, 3, 4, 5];
+	let random = Math.floor(Math.random() * images.length);
+	const div = document.createElement("div");
+	div.classList.add("person");
+	div.innerHTML = `<img
+	src="img/person-${random}.jpeg"
+	alt="person"
+	class="person_thumbnail"
+/>
+<h4 class="person__name">${customer.name}</h4>
+<h4 class="person__last-name">${customer.lastname}</h4>`;
+	document.querySelector(".drink-card__list").appendChild(div);
+};
+
+function Customer(name, lastname, email) {
+	(this.name = name), (this.lastname = lastname), (this.email = email);
+}
